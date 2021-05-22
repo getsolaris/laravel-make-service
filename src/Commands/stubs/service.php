47904f8a5070eb13377a0a23a5545fbfec345902
Mingeun Kim <mingeun.k.k@gmail.com>
2021-05-22 10:11:45 +0900

    chore: change package name and remove require-dev

---
+++ src/Commands/stubs/service.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ServiceStub;
+
+use App\Services\BaseService;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+class ServiceStub extends BaseService
+{
+    
+}
