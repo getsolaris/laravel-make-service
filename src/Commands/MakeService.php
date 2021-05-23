c35547c018a13186073e99d5dcc57bfd8c1b2b97
Mingeun Kim <mingeun.k.k@gmail.com>
2021-05-23 17:33:07 +0900

    feat: create make service command

---
+++ src/Commands/MakeService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Getsolaris\LaravelMakeService\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class MakeService extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Determine if the class already exists.
+     *
+     * @param  string  $rawName
+     * @return bool
+     */
+    protected function alreadyExists($rawName)
+    {
+        return class_exists($rawName);
+    }
+
+    /**
+     * @return string
+     */
+    private function getStub(): string
+    {
+        return __DIR__ . '/src/stubs/service.stub';
+    }
+
+    /**
+     * @param $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Services';
+    }
+}
