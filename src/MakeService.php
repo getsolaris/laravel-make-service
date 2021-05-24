6c3b6604f18236b6be3cf02b3f6334375650bc9f
Mingeun Kim <mingeun.k.k@gmail.com>
2021-05-24 21:29:38 +0900

    feat: command option contract

--- src/MakeService.php
+++ src/MakeService.php
@@ -4,6 +4,11 @@ namespace Getsolaris\LaravelMakeService;
 
 use Illuminate\Console\GeneratorCommand;
 
+/**
+ * Class MakeService
+ * @package Getsolaris\LaravelMakeService
+ * @author  getsolaris (https://github.com/getsolaris)
+ */
 class MakeService extends GeneratorCommand
 {
     /**
@@ -11,14 +16,14 @@ class MakeService extends GeneratorCommand
      *
      * @var string
      */
-    protected $signature = 'make:service {name}';
+    protected $signature = 'make:service {name : Create a service class} {--c : Create a service contract}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new service class';
+    protected $description = 'Create a new service class and contract';
 
     /**
      * The type of class being generated.
@@ -26,13 +31,107 @@ class MakeService extends GeneratorCommand
      * @var string
      */
     protected $type = 'Service';
-    
+
+    protected function getStub() { }
+
+    /**
+     * @param bool $isContract
+     * @return string
+     */
+    protected function getServiceStub(bool $isContract): string
+    {
+        if ($isContract) {
+            return __DIR__ . '/Stubs/service.stub';
+        }
+
+        return __DIR__ . '/Stubs/service.origin.stub';
+    }
+
     /**
      * @return string
      */
-    protected function getStub(): string
+    protected function getServiceContractStub(): string
     {
-        return __DIR__ . '/Stubs/service.stub';
+        return __DIR__ . '/Stubs/service.contract.stub';
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool|null
+     *
+     * @see \Illuminate\Console\GeneratorCommand
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function handle()
+    {
+        if ($this->isReservedName($this->getNameInput())) {
+            $this->error('The name "'.$this->getNameInput().'" is reserved by PHP.');
+
+            return false;
+        }
+
+        $name = $this->qualifyClass($this->getNameInput());
+
+        $path = $this->getPath($name);
+
+        if ((! $this->hasOption('force') ||
+                ! $this->option('force')) &&
+            $this->alreadyExists($this->getNameInput())) {
+            $this->error($this->type.' already exists!');
+
+            return false;
+        }
+
+        $this->makeDirectory($path);
+        $isContract = $this->option('c');
+
+        $this->files->put($path, $this->sortImports(
+            $this->buildServiceClass($name, $isContract)
+        ));
+        $message = $this->type;
+
+        // Whether to create contract
+        if ($isContract) {
+            $contractName = str_replace($this->getNameInput(), $this->getNameInput() . 'Contract.php', $this->getNameInput());
+            $contractPath = str_replace($this->getNameInput() . '.php', 'Contracts/', $path);
+            $this->makeDirectory($contractPath . $contractName);
+            $this->files->put($contractPath . $contractName, $this->sortImports($this->buildServiceContractInterface($this->getNameInput())));
+            $message .= ' and Contract';
+        }
+
+        $this->info($message . ' created successfully.');
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param string $name
+     * @param $isContract
+     * @return string
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildServiceClass($name, $isContract): string
+    {
+        $stub = $this->files->get($this->getServiceStub($isContract));
+
+        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildServiceContractInterface($name): string
+    {
+        $stub = $this->files->get($this->getServiceContractStub());
+
+        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
     }
 
     /**
